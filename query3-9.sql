29e72cfcc1ae9185f153abdc194bef56f2925783
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-04-20 18:27:59 +0000

    task 3-9

--- query3-9.sql
+++ query3-9.sql
@@ -1,5 +1,6 @@
-Select staff_id, given_name, family_name
+SELECT staff_id, given_name, family_name
 FROM Teachers
 INNER JOIN Subjects ON Subjects.teacher_id = Teachers.staff_id
 INNER JOIN Enrolments on Enrolments.subject_code = Subjects.subject_code
-WHERE student_id = 'a1111113';
+WHERE student_id = 'a1111113'
+AND Teachers.faculty = 'ECMS';
